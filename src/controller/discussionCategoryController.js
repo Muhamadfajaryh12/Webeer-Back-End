7632934a63d6acb92b1062adecba2185a17510b6
LuthfiKhairullah <5luthfi.khairullah@gmail.com>
2022-11-08 16:13:03 +0700

    memperbaiki discussion category

--- src/controller/discussionCategoryController.js
+++ src/controller/discussionCategoryController.js
@@ -1,13 +1,30 @@
+const { ObjectId } = require("mongodb")
+const Discussion = require("../models/discussion")
 const Category = require("../models/discussionCategory")
 
 const createCategory = async (req, res, next) => {
+    const { id } = req.params;
+    const categoryID = new ObjectId();
+
     const{
         name
     } = req.body
-    const newCategory = new Category({
-        name
+
+    const discussion = await Discussion.findOne({
+        _id: id
     })
+
+    const newCategory = new Category({
+        _id: categoryID,
+        name,
+        discussions: id
+    })
+
     const category = await newCategory.save();
+
+    discussion.categories.push(categoryID);
+    await discussion.save()
+    
     res.status(201).json({
         success: true,
         data: category,
--- src/routes/discussionCategoryRouter.js
+++ src/routes/discussionCategoryRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const upload = require('multer')();
 const {
     createCategory,
     getCategories
 } = require("../controller/discussionCategoryController");
 const asyncHandler = require("../utils/asyncHandler");
-router.route("/").post(asyncHandler(createCategory));
+router.route("/:id").post(upload.any(), asyncHandler(createCategory));
 router.route("/").get(asyncHandler(getCategories));
 module.exports = router;
