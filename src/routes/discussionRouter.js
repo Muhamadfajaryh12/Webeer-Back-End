955cd0f63d69397976a35d1850e6c07f08e6523f
Muhamadfajaryh12 <2010631170090@student.unsika.ac.id>
2022-11-08 08:03:41 +0700

    fix form

--- src/routes/discussionRouter.js
+++ src/routes/discussionRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const upload = require('multer')()
 const {
     createDiscussion,
     getAllDiscussion,
@@ -7,7 +8,7 @@ const {
     getDiscussion
 } = require("../controller/discussionController");
 const asyncHandler = require("../utils/asyncHandler");
-router.route("/").post(asyncHandler(createDiscussion));
+router.route("/").post(upload.any(),asyncHandler(createDiscussion));
 router.route("/").get(asyncHandler(getAllDiscussion));
 router.route("/:id").get(asyncHandler(getDiscussion));
 router.route("/:id").delete(asyncHandler(deleteDiscussion));
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const upload = require('multer')()
 const {
     createUser,
     getUser
 } = require("../controller/userController");
 const asyncHandler = require("../utils/asyncHandler");
-router.route("/").post(asyncHandler(createUser));
+router.route("/").post(upload.any(),asyncHandler(createUser));
 router.route("/all").get(asyncHandler(getUser))
 module.exports = router;
