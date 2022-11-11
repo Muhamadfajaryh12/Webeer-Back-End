cc1cf8a278d2b079e11ff4ef706dc3c3c5b42ca8
LuthfiKhairullah <5luthfi.khairullah@gmail.com>
2022-11-11 07:24:35 +0700

    memperbaiki conflict

--- src/controller/discussionController.js
+++ src/controller/discussionController.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 const { ObjectId } = require('mongodb');
-=======
->>>>>>> 61ca04fce3e76e4711951db470b5f0ca01ad9161
 const Discussion = require('../models/discussion.js');
 const Category = require('../models/discussionCategory.js');
 const DiscussionReply = require('../models/discussionReply.js');
@@ -12,20 +9,12 @@ const createDiscussion = async(req,res) =>{
         name,
         title,
         discussion,
-<<<<<<< HEAD
         categories
     } = req.body;
-=======
-    } = req.body
->>>>>>> 61ca04fce3e76e4711951db470b5f0ca01ad9161
 
     const newDate = new Date();
     const monthID = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
     const date = `${newDate.getDate()} ${monthID[newDate.getMonth()]} ${newDate.getFullYear()}`;
-<<<<<<< HEAD
-=======
-    const categories =[];
->>>>>>> 61ca04fce3e76e4711951db470b5f0ca01ad9161
     const reply = [];
     const isSolved = false;
     if(typeof(categories) === 'object') {
