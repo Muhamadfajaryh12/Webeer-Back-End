aff89d02cb13b603251f7cd9defbe0505aeda8dd
Muhamadfajaryh12 <2010631170090@student.unsika.ac.id>
2022-11-30 11:43:08 +0700

    expire opt

--- src/models/otpUser.js
+++ src/models/otpUser.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const OTPUserSchema = new mongoose.Schema({
     idUser :String,
     OTP :String,
-})
+    createdAt:Date,
+    expiresAt:Date,
+},
+)
 const OTPUser = new mongoose.model("OTPUser",OTPUserSchema)
 module.exports = OTPUser;
