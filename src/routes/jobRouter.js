05057a041b1483c83791b0e0aaef0538f191e3db
LuthfiKhairullah <5luthfi.khairullah@gmail.com>
2022-12-10 20:35:32 +0700

    fix

--- src/routes/jobRouter.js
+++ src/routes/jobRouter.js
@@ -15,12 +15,12 @@ const {
 const asyncHandler = require('../utils/asyncHandler');
 const verifyToken = require('../middleware/auth');
 
-router.route('/').post(uploadImg, asyncHandler(createJob));
+router.route('/').post(verifyToken, uploadImg, asyncHandler(createJob));
 router.route('/all').get(asyncHandler(getJob));
 router.route('/detail/:id').get(asyncHandler(getJobDetail));
 router.route('/').get(asyncHandler(getJobName));
-router.route('/company').get(asyncHandler(getCompanyJob));
-router.route('/:id').delete(asyncHandler(deleteJob));
-router.route('/:id').put(uploadImg, asyncHandler(editJob));
-router.route('/:id').get(asyncHandler(getJobOther));
+router.route('/company').get(verifyToken, asyncHandler(getCompanyJob));
+router.route('/:id').delete(verifyToken, asyncHandler(deleteJob));
+router.route('/:id').put(verifyToken, uploadImg, asyncHandler(editJob));
+router.route('/:id').get(verifyToken, asyncHandler(getJobOther));
 module.exports = router;
